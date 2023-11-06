8b7f8cf786b972a8a055e0f4e7f0d10359fe3c17
shalom-nganga <minnomacharia@gmail.com>
2023-11-06 22:32:54 +0300

    complete assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { format } from "date-fns";
 import ExampleComponent from "./ExampleComponent";
-
+import TestComponent from "./TestComponent";
 // Add your code own within the return statement
 function App() {
   return (
@@ -14,6 +14,7 @@ function App() {
         self-contained chunks of JSX.
       </p>
       <ExampleComponent />
+      <TestComponent />
     </div>
   );
 }
